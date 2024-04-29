af7e1a623a6e8cdacd7b1ca083324488e085f902
SokolovVP <Sokolovvp02@mail.ru>
2024-04-29 21:19:49 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+result = cv2.imread('D:\универ (D)\\4 КУРС\диплом\Снимки печени\\2023-5-16-17-20-20.png')
+
+#plt.figure(figsize=(40, 40))
+#plt.imshow(result)
+#plt.tight_layout()
+
+#plt.show()
